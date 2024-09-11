1de61b8f8530f2f1187ac88b01b3a61b268f3586
omomo <omomo1017@gmail.com>
2024-09-11 09:59:05 +0900

    updated Chapter 10

--- src/Chapter10/App.jsx
+++ src/Chapter10/App.jsx
@@ -1,30 +1,63 @@
-import React from 'react'
+import PropTypes from "prop-types";
+import React from "react";
 
 const App = () => {
   return (
     <div>
-      <StarRating/>
-      <Star />
+      <StarRating />
     </div>
-  )
-}
+  );
+};
 
-function StarRating({maxRating = 5}) {
+function StarRating({ maxRating = 5 }) {
+  const [rating, setRating] = React.useState(0);
+  const [tmpRating, setTmpRating] = React.useState(0);
+
+  function handleRate(rating) {
+    setRating(rating);
+  }
+
   return (
     <div>
       <div>
-        {Array.from({length: maxRating}, (_, index) => (
-            <span>S{index+1}</span>))}
+        {Array.from({ length: maxRating }, (_, index) => (
+          <Star
+            key={index}
+            onRate={() => handleRate(index + 1)}
+            onHoverIn={() => setTmpRating(index + 1)}
+            onHoverOut={() => setTmpRating(0)}
+            full={tmpRating ? tmpRating >= index + 1 : rating >= index + 1}
+          />
+        ))}
       </div>
+      <p>{tmpRating || rating || ""}</p>
     </div>
-  )
+  );
 }
 
-function Star(){
-    const [isActivated, setIsActivated] = React.useState(false)
-    return (
-        <span role='button' onClick={()=>setIsActivated(!isActivated)}>{isActivated ? "☆" : "★"}</span>
-    )
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+};
+
+function Star({ onRate, onHoverIn, onHoverOut, full }) {
+  // const [isActivated, setIsActivated] = React.useState(false)
+  return (
+    <span
+      role="button"
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
+      {full ? "★" : "☆"}
+    </span>
+  );
 }
 
-export default App
+Star.propTypes = {
+  onRate: PropTypes.func.isRequired,
+  onHoverIn: PropTypes.func.isRequired,
+  onHoverOut: PropTypes.func.isRequired,
+  full: PropTypes.bool,
+};
+
+export default App;
