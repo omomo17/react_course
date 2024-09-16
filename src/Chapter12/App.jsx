fa0690a219580e4881efca3c4224c8e761984980
momo <omomo061017@gmail.com>
2024-09-16 23:54:51 +0900

    Chapter 12 updated

--- src/Chapter12/App.jsx
+++ src/Chapter12/App.jsx
@@ -34,7 +34,7 @@ export default function App() {
 
     return (
         <div>
-            <input type="text" value={amount} onChange={e => setAmount(Number(e.target.value))} disabled={isLoading} />
+            <input type="text" value={amount} onChange={e => setAmount(Number(e.target.value))} disabled={false/*isLoading*/} />
             <select value={fromCurrency} onChange={e => setFromCurrency(e.target.value)} disabled={isLoading}>
                 <option value="USD">USD</option>
                 <option value="EUR">EUR</option>
